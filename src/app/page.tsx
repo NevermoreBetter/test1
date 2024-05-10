05fb9f328d6cdcc874dd24b696602bf74209a681
NevermoreBetter <jepaxdd@gmail.com>
2024-05-10 20:40:32 +0300

    m

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { getEvents } from "@/actions/getEvents";
 import EventsList from "./_components/events-list";
-import { getFromApi } from "@/actions/getFromApi";
 
 export default async function Home() {
  const events = await getEvents();
